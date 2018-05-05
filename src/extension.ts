ed14eb008b82bb590edf3ee9c09145a4786a2ad9
Rohith Rajan <rohithkrajan@hotmail.com>
2018-05-05 23:55:06 +0530

    added quick implemenation for implements,extends,createObject etc.

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from "path";
+import * as fs from "fs";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -25,31 +26,58 @@ export function activate(context: vscode.ExtensionContext) {
         let textLine = editor.document.lineAt(selection.start.line);
 
 
-        var templateRegex:RegExp=/template=\"(.*)\"/;
-        var newRegex:RegExp=/new\D([a-zA-Z0-9\.]*)\(.*/;
-        var match = templateRegex.exec(textLine.text);   
-        var currentDirName=path.dirname(editor.document.uri.fsPath);      
-        var gotoDocPath:vscode.Uri|null=null;
-        if(match!==null)
-        {
-            gotoDocPath= vscode.Uri.file(currentDirName+"\\"+match[1]);       
-            
+        var templateRegex: RegExp = /template=\"(.*)\"/;
+        var newRegex: RegExp = /new\D([a-zA-Z0-9\.]*)\(.*/;
+        var extendsRegex: RegExp = /\s+extends\s?=\s?[\'\"]([^\s]+)[\'\"]/;
+        var implementsRegex: RegExp = /\s+implements\s?=\s?[\'\"]([^\s]+)[\'\"]/;
+        var createObjRegex:RegExp=/createObject\(\"component\"\,\"([^\s]+)\"/;
+        var match = templateRegex.exec(textLine.text);
+        var currentDirName = path.dirname(editor.document.uri.fsPath);
+        var gotoDocPath: vscode.Uri | null = null;
+        if (match !== null) {
+            gotoDocPath = vscode.Uri.file(currentDirName + "\\" + match[1]);
+
         }
-        match=newRegex.exec(textLine.text);
-        if(match!==null)
-        {
-            var componentName:string=match[1];
-            var componentpath:string=componentName.split(".").join("\\");
-            gotoDocPath=vscode.Uri.file(vscode.workspace.rootPath+"\\"+componentpath+".cfc");
+        else {
+            match = newRegex.exec(textLine.text);
+            if (match === null) {
+                match = extendsRegex.exec(textLine.text);
+            }
+            if(match===null)
+            {
+                match=implementsRegex.exec(textLine.text);
+            }
+            if(match===null)
+            {
+                match=createObjRegex.exec(textLine.text);
+            }
+            if (match !== null) {
+                var componentName: string = match[1];
+                var componentpath: string = componentName;
+                if (componentName.indexOf(".") !== -1) {
+                    componentpath = componentName.split(".").join("\\");
+                }
+                var componentFilePath = vscode.workspace.rootPath + "\\" + componentpath + ".cfc";
+
+                try {
+                    fs.accessSync(componentFilePath);
+                } catch (error) {
+                    componentFilePath = currentDirName + "\\" + componentpath + ".cfc";
+                }
+                
+               
+
+                gotoDocPath = vscode.Uri.file(componentFilePath);
+            }
         }
-        
-        if(gotoDocPath!==null)
-        {
+
+
+        if (gotoDocPath !== null) {
             vscode.workspace.openTextDocument(gotoDocPath)
-            .then(doc => vscode.window.showTextDocument(doc),
-            error=>vscode.window.showInformationMessage(error));
+                .then(doc => vscode.window.showTextDocument(doc),
+                    error => vscode.window.showInformationMessage(error));
         }
-       
+
         // Display a message box to the user
         //vscode.window.showInformationMessage('Selected characters: ' + text.length);
         // Display a message box to the user
